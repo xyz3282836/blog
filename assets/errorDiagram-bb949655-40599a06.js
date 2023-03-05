384b3e6f2638a52376892d8ffb4b58037642ae46
liuruizhou <liuruizhou@bilibili.com>
2023-03-05 14:28:43 +0800

    update doc

--- assets/errorDiagram-bb949655-40599a06.js
+++
@@ -1,1 +0,0 @@
-import{aC as r}from"./mermaid.core-a41b7048.js";import"./app-f485299a.js";import"./framework-8bc9c5c2.js";const e=()=>"",t=e,p={db:{clear:()=>{}},styles:t,renderer:r,parser:{parser:{yy:{}},parse:()=>{}},init:()=>{}};export{p as diagram};
